6ecb9bf605499df3195a44c8a8a63208a6025757
Themyscira <takeda.syouhei@gmail.com>
2019-12-31 23:17:58 +0900

    Edit  Player Move

--- UnityDevelop/Assets/Resources/scripts/PlayerMove.cs
+++ UnityDevelop/Assets/Resources/scripts/PlayerMove.cs
@@ -18,6 +18,21 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     //   
+        //  player moves
+        CharacterController controller_ = GetComponent<CharacterController>();
+
+        if (controller_.isGrounded)
+        {
+            move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            move_direction = transform.TransformDirection(move_direction);
+            move_direction *= speed;
+            if (Input.GetButton("Jump"))
+            {
+                move_direction.y = jump_speed;
+            }
+
+            move_direction.y = gravity * Time.deltaTime;
+            controller_.Move(move_direction * Time.deltaTime);
+        }
     }
 }
