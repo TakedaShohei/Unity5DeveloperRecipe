ba45b9092c50d2758366513c0358a8c6ee62eebd
Themyscira <takeda.syouhei@gmail.com>
2020-01-16 01:04:01 +0900

    Change rotation

--- UnityDevelop/Assets/Resources/scripts/PlayerMove.cs
+++ UnityDevelop/Assets/Resources/scripts/PlayerMove.cs
@@ -30,54 +30,6 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        //　CharacterControllerのコライダで接地が確認出来ない場合
-        if (!controller_.isGrounded)
-        {
-
-            if (Physics.Linecast(ray_transform.position, (ray_transform.position - transform.up * ray_range)))
-            {
-                isGround_ = true;
-            }
-            else
-            {
-                isGround_ = false;
-            }
-
-            //　接地確認用にレイを視覚化
-            Debug.DrawLine(ray_transform.position, (ray_transform.position - transform.up * ray_range), Color.red);
-
-        }
-        */
-
-        if (controller_.isGrounded || isGround_)
-        {
-            /*
-            //　地面に接地してる時は速度を初期化
-            if (controller_.isGrounded)
-            { //　レイを飛ばして接地確認の場合は重力だけは働かせておく、前後左右は初期化
-                move_direction = Vector3.zero;
-            } else {
-                move_direction = new Vector3(0f, move_direction.y, 0f);
-
-            }
-            
-            move_direction = new Vector3(0f, move_direction.y, 0f);
-
-            move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-            move_direction = transform.TransformDirection(move_direction);
-            move_direction *= speed;
-            if (Input.GetButton("Jump"))
-            {
-                move_direction.y = jump_speed;
-            }
-
-            move_direction.y = gravity * Time.deltaTime;
-            controller_.Move(move_direction * Time.deltaTime);
-            */
-        }
-
-
         move_direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         move_direction = transform.TransformDirection(move_direction);
         move_direction *= speed;
@@ -94,6 +46,12 @@ public class PlayerMove : MonoBehaviour
         }
         
         controller_.Move(move_direction * Time.deltaTime);
+        PlayerRotate();
+    }
+
+    public void PlayerRotate()
+    {
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
     }
   
 }
